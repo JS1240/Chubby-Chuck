ba8ebc8e9057293a072bd09b1403b0de80a07125
Jure Sunic <juresunic@hotmail.com>
2025-01-09 22:40:25 +0100

    Set the main parts of the app.    sStarted with the nav bar and now on the hero section. the section needs imporvements because there are no videos that are made for the web

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { bagImg, chubbyImg, searchImg } from "../utils";
+import { navLists } from '../constants';
+
+const Navbar = () => {
+  return (
+    <header className="w-full py-5 sm:px10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={chubbyImg} alt="Chubby" width={14} height={18} />
+
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div key={nav} className="px-5 text-sm cursor-pointer text-grey hover:text-blue transition-all">
+              {nav}
+            </div>
+            ))}
+        </div>
+
+        <div className="flex items-baseline gap-7 max:smjustify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
